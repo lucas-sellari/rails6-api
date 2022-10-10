3b27f99af2670593d685c00f830d9b76476f97df
lucas-sellari <lucasmiguel448@gmail.com>
2022-10-10 00:28:19 -0300

    add author model and author/book association

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_08_200915) do
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
+ActiveRecord::Schema[7.0].define(version: 2022_10_10_031810) do
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
 end
