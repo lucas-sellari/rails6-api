7b551509448f94e83e6f4bba22cc96e2cbdd8ec3
lucas-sellari <lucasmiguel448@gmail.com>
2022-10-15 20:35:51 -0300

    add tests foractive job class and book controller

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,9 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::BooksController, type: :controller do
-  it "has a max limit of 100" do
-    expect(Book).to receive(:limit).with(100).and_call_original
+  describe "GET index" do
+    it "has a max limit of 100" do
+      expect(Book).to receive(:limit).with(100).and_call_original
 
-    get :index, params: { limit: 999 }
+      get :index, params: { limit: 999 }
+    end
+  end
+
+  describe "POST create" do
+    let(:book_title) { "Harry Pottery" }
+
+    it "calls UpdateSkuJob with correct params" do
+      expect(UpdateSkuJob).to receive(:perform_later).with(book_title)
+
+      post :create, params: {
+        author: { first_name: "JK", last_name: "Drugs", age: 55 },
+        book: { title: "Harry Pottery" }
+      }
+    end
   end
 end
--- spec/jobs/update_sku_job_spec.rb
+++ spec/jobs/update_sku_job_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe UpdateSkuJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:book_title) { "book name" }
+
+  before do
+    # this line mocks the request
+    allow(Net::HTTP).to receive(:start).and_return(true)
+  end
+
+  it "calls SKU service with correct params" do
+    expect_any_instance_of(Net::HTTP::Post).to receive(:body=).with(
+      { sku: "123", title: book_title }.to_json
+    )
+
+    described_class.perform_now("book name")
+  end
 end
